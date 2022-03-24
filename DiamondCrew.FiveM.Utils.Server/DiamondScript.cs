dc372d9c668dd828d4ced18d77bff27826490b3c
ThePlay3r <radomil.janicek@gmail.com>
2022-03-24 16:28:22 +0100

    feat: add server

---
+++ DiamondCrew.FiveM.Utils.Server/DiamondScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CitizenFX.Core;
+
+namespace DiamondCrew.FiveM.Utils.Server
+{
+    public abstract class DiamondScript : BaseScript
+    {
+        protected DiamondScript()
+        {
+            SetupListeners();
+        }
+
+        protected abstract void OnStart();
+
+        protected abstract void OnStop();
+        private void SetupListeners()
+        {
+            EventHandlers["onResourceStart"] += new Action(OnStart);
+            EventHandlers["onResourceStop"] += new Action(OnStop);
+        }
+    }
+}
