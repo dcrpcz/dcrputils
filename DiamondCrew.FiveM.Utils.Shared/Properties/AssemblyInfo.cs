a0a3db0e6947ff4deeb31c7eb480e24a48ac43ee
ThePlay3r <radomil.janicek@gmail.com>
2022-03-30 14:43:28 +0200

    fix author and desc

--- DiamondCrew.FiveM.Utils.Shared/Properties/AssemblyInfo.cs
+++ DiamondCrew.FiveM.Utils.Shared/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DiamondCrew.FiveM.Utils.Shared")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utilities for easier FiveM development in C#.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DiamondCrew")]
 [assembly: AssemblyProduct("DiamondCrew.FiveM.Utils.Shared")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © DiamondCrew 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
